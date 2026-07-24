c46edc1125b991b42eaf708440e9846c7176bdd5
Jay-Gujar-1207 <jaygujar120706@gmail.com>
2026-07-24 18:17:05 +0530

    added comments to understand process

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,20 +12,24 @@ from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
+    # it will store paths where raw, train, and test datasets will be saved.
+    train_data_path: str=os.path.join('artifacts',"train.csv")   
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
+    # Initializes the DataIngestion class and loads configuration
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or componenet")
         try:
+
             df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
+            # create artifact directory if not exists
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True)  
@@ -54,5 +58,7 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()       # Create an object of the DataTransformation class
+
+    # Apply preprocessing on the train and test datasets, save the preprocessor object
+    data_transformation.initiate_data_transformation(train_data,test_data) 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,6 +16,7 @@ from src.utils import save_object
 
 @dataclass 
 class DataTransformationConfig:
+    # path to save the preprocessor file
     preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")    
 
 class DataTransformation:
@@ -23,6 +24,7 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        # for creating preprocesssing pipeline
         try:
             numerical_columns=["writing_score","reading_score"]
             categorical_columns=[
@@ -50,7 +52,7 @@ class DataTransformation:
 
             logging.info(f"Categorical columns: {categorical_columns}")
 
-
+            ## combine preprocessing and column transformation
             preprocessor=ColumnTransformer(
                 [
                     ("num_pipeline",num_pipeline,numerical_columns),
@@ -63,7 +65,7 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
 
-
+    #  transformation of data begins here
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
@@ -73,9 +75,10 @@ class DataTransformation:
             logging.info("Read train and test data completed")
             logging.info("obtaining preprocessing object")
 
+            # Get the preprocessing pipeline
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
+            target_column_name="math_score"   # dependent feature
             numerical_columns=["writing_score","reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
@@ -85,7 +88,7 @@ class DataTransformation:
             target_feature_test_df=test_df[target_column_name]
 
             logging.info("Applying preprocessing object on training dataframe and testing dataframe")
-
+            
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
